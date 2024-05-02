3091e59ee73258a5d5860de774a36daa93d17b8a
Jon0945 <john.gullne@gmail.com>
2024-05-02 12:39:31 +0200

    Workshop Progress

--- sql_1_select.sql
+++ sql_1_select.sql
@@ -12,60 +12,61 @@
 # Table: CountryLanguage
 # Columns: CountryCode, Language, IsOfficial,Percentage
 #
-#
+# USE world;
 # 1: Get a query to return "Hello World", 123
 # (Hint: 1 row, 2 columns)
 SELECT "hello world" as message , 123 as id;
 #
 # 2: Get everything from the city table
 # (Hint: Many many rows)
-#
+SELECT * from city;
 #
 # 3: Get everything on the cities whose district is "aceh"
 # (Hint: 2 rows)
-#
+SELECT * from city WHERE district = "aceh";
 #
 # 4: Get only the name of the cities where the countrycode is "bfa"
-#
+SELECT Name FROM city WHERE CountryCode = "bfa";
 #
 # 5: Get both the name and district of the cities where the countrycode is "tto"
-#
+SELECT Name, District FROM city WHERE CountryCode = "tto";
 #
 # 6: Get the name and district named as nm,dist from the cities where the countrycode is "arm"
-#
+SELECT Name AS Nm, District as dist FROM city WHERE CountryCode="arm";
 #
 # 7: Get the cities with a name that starts with "bor"
-#
+SELECT Name AS CityName from city where Name LIKE 'bor%';
 #
 # 8: Get the cities with a name that contains the string "orto"
-#
+SELECT * FROM city WHERE Name LIKE "%orto%";
 #
 # 9: Get the cities that has a population below 1000
-#
+SELECT * FROM city WHERE population<1000;
 #
 # 10: Get the unique countrycodes from the cities that has a population below 1000
-#
+SELECT DISTINCT CountryCode FROM city WHERE population<1000;
 #
 # 11: Get the cities with the countrycode UKR that has more than 1000000 (one million) in population
-#
+SELECT Name FROM city WHERE CountryCode LIKE "UKR" AND population>1000000;
 #
 # 12: Get the cities with a population of below 200 or above 9500000 (9.5 million)
-#
+SELECT Name FROM city WHERE population<200 OR population>9500000;
 #
 # 13: Get the cities with the countrycodes TJK, MRT, AND, PNG, SJM
-#
+SELECT * FROM city WHERE CountryCode IN ( 'TJK', 'MRT', 'AND', 'PNG', 'SJM'); 
 #
 # 14: Get the cities with a population between 200 and 700 inclusive
-#
+SELECT * from city WHERE population >= 200 AND population <=700;
+SELECT * from city WHERE population BETWEEN 200 and 700;
 #
 # 15: Get the countries with a population between 8000 and 20000 inclusive
-#
+SELECT * from country WHERE Population BETWEEN 8000 AND 20000;
 #
 # 16: Get the name of the countries with a independence year (indepyear) before year 0
-#
+SELECT * from country WHERE IndepYear <0;
 #
 # 17: Get the countries that has no recorded independence year and a population above 1000000
-#
+SELECT * from country WHERE IndepYear IS null AND population>1000000;
 #
 # 18: Get countries with a SurfaceArea below 10 and a defined LifeExpectancy
-#
+SELECT * from country WHERE LifeExpectancy IS NOT null AND SurfaceArea<10; 
--- sql_2_join.sql
+++ sql_2_join.sql
@@ -3,49 +3,49 @@
 
 #
 # 1: Get the cities with a name starting with ping sorted by their population with the least populated cities first
-#
+SELECT * FROM city WHERE Name LIKE "ping%" ORDER BY Population ASC;
 #
 # 2: Get the cities with a name starting with ran sorted by their population with the most populated cities first
-#
+SELECT * FROM city WHERE Name LIKE "ran%" ORDER BY Population DESC;
 #
 # 3: Count all cities
-#
+SELECT COUNT(*) AS Total_Cities from city;
 #
 # 4: Get the average population of all cities
-#
+SELECT AVG(Population) AS Average_Population from city;
 #
 # 5: Get the biggest population found in any of the cities
-#
+SELECT * from city ORDER BY Population DESC LIMIT 1;
 #
 # 6: Get the smallest population found in any of the cities
-#
+SELECT * from city ORDER BY Population ASC LIMIT 1;
 #
 # 7: Sum the population of all cities with a population below 10000
-#
+SELECT SUM(Population) AS Total_Population FROM city WHERE Population <10000;
 #
 # 8: Count the cities with the countrycodes MOZ and VNM
-#
+SELECT COUNT(*) FROM City WHERE CountryCode IN('MOZ', 'VNM');
 #
 # 9: Get individual count of cities for the countrycodes MOZ and VNM
-#
+SELECT CountryCode,Count(*) FROM City WHERE CountryCode IN('MOZ', 'VNM') group by CountryCode; 
 #
 # 10: Get average population of cities in MOZ and VNM
-#
+SELECT Name,AVG(Population) FROM City WHERE CountryCode IN('MOZ', 'VNM') Group by Name;
 #
 # 11: Get the countrycodes with more than 200 cities
-#
+SELECT CountryCode,Count(CountryCode) AS NumberOfCities FROM City Group by CountryCode HAVING NumberOfCities >200; 
 #
 # 12: Get the countrycodes with more than 200 cities ordered by city count
-#
+SELECT CountryCode,Count(CountryCode) AS NumberOfCities FROM City Group by CountryCode HAVING NumberOfCities >200 ORDER BY NumberOfCities ASC; 
 #
 # 13: What language(s) is spoken in the city with a population between 400 and 500 ?
-#
+SELECT language from countrylanguage cl JOIN city ct ON cl.CountryCode =ct.CountryCode WHERE ct.Population BETWEEN 400 and 500;
 #
 # 14: What are the name(s) of the cities with a population between 500 and 600 people and the language(s) spoken in them
-#
+SELECT Name,language,Population from countrylanguage cl JOIN city ct ON cl.CountryCode =ct.CountryCode WHERE ct.Population BETWEEN 500 and 600 group by ct.Name;
 #
 # 15: What names of the cities are in the same country as the city with a population of 122199 (including the that city itself)
-#
+
 #
 # 16: What names of the cities are in the same country as the city with a population of 122199 (excluding the that city itself)
 #
